b85a30eab340b9c7de0eb5c32eb2efaac0a8fee6
Chen <m10902141@gapps.ntust.edu.tw>
2021-10-07 19:23:26 +0800

    update

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ def load_training_data(path='training_label.txt'):
     # 把 training 時需要的 data 讀進來
     # 如果是 'training_label.txt'，需要讀取 label，如果是 'training_nolabel.txt'，不需要讀取 label
     if 'training_label' in path:
-        with open(path, 'r') as f:
+        with open(path, 'r',encoding="utf-8") as f:
             lines = f.readlines()
             lines = [line.strip('\n').split(' ') for line in lines]
         x = [line[2:] for line in lines]
         y = [line[0] for line in lines]
         return x, y
     else:
-        with open(path, 'r') as f:
+        with open(path, 'r',encoding="utf-8") as f:
             lines = f.readlines()
             x = [line.strip('\n').split(' ') for line in lines]
         return x
 
 def load_testing_data(path='testing_data'):
     # 把 testing 時需要的 data 讀進來
-    with open(path, 'r') as f:
+    with open(path, 'r',encoding="utf-8") as f:
         lines = f.readlines()
         X = ["".join(line.strip('\n').split(",")[1:]).strip() for line in lines[1:]]
         X = [sen.split(' ') for sen in X]
@@ -56,7 +56,7 @@ from gensim.models import word2vec
 
 def train_word2vec(x):
     # 訓練 word to vector 的 word embedding
-    model = word2vec.Word2Vec(x, size=250, window=5, min_count=5, workers=12, iter=10, sg=1)
+    model = word2vec.Word2Vec(x, vector_size=250, window=5, min_count=5, workers=12, epochs=10, sg=1)
     return model
 
 
